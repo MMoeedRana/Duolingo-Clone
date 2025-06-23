b31594c50b123f77090399955e9bb4a36aae1ef3
M Moeed Rana <mmoeedrana1@gmail.com>
2025-06-23 13:37:25 +0500

    Updated route.ts file with new changes

--- app/api/challengeOptions/[challengeOptionId]/route.ts
+++ app/api/challengeOptions/[challengeOptionId]/route.ts
@@ -2,65 +2,43 @@ import db from "@/db/drizzle";
 import { challengeOptions } from "@/db/schema";
 import { isAdmin } from "@/lib/admin";
 import { eq } from "drizzle-orm";
-import { NextRequest, NextResponse } from "next/server";
-
-type Context = {
-  params: {
-    challengeOptionId: string;
-  };
-};
+import { NextResponse } from "next/server";
 
 export const GET = async (
-  req: NextRequest,
-  { params }: Context
+  req: Request,
+  { params }: { params: { challengeOptionId: number } },
 ) => {
-  if (!isAdmin()) {
+  if(!isAdmin()) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
-
-  const challengeOptionId = parseInt(params.challengeOptionId, 10);
-
   const data = await db.query.challengeOptions.findFirst({
-    where: eq(challengeOptions.id, challengeOptionId),
+    where: eq(challengeOptions.id, params.challengeOptionId),
   });
-
   return NextResponse.json(data);
 };
 
 export const PUT = async (
-  req: NextRequest,
-  { params }: Context
+  req: Request,
+  { params }: { params: { challengeOptionId: number } },
 ) => {
-  if (!isAdmin()) {
+  if(!isAdmin()) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
-
-  const challengeOptionId = parseInt(params.challengeOptionId, 10);
   const body = await req.json();
-
-  const data = await db.update(challengeOptions)
-    .set({
-      ...body,
-    })
-    .where(eq(challengeOptions.id, challengeOptionId))
-    .returning();
-
+  const data = await db.update(challengeOptions).set({
+    ...body,
+  }).where(eq(challengeOptions.id, params.challengeOptionId)).returning();
   return NextResponse.json(data[0]);
 };
 
 export const DELETE = async (
-  req: NextRequest,
-  { params }: Context
+  req: Request,
+  { params }: { params: { challengeOptionId: number } },
 ) => {
-  if (!isAdmin()) {
+  if(!isAdmin()) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
-
-  const challengeOptionId = parseInt(params.challengeOptionId, 10);
-
   const data = await db.delete(challengeOptions)
-    .where(eq(challengeOptions.id, challengeOptionId))
-    .returning();
-
+  .where(eq(challengeOptions.id, params.challengeOptionId)).returning();
   return NextResponse.json(data[0]);
 };
